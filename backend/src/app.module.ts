2f2659e2c3c80fdd282672b79e3be48d0f3ff7da
Julien Constant <julienconstant190@gmail.com>
2022-05-06 16:22:24 +0200

    yeet

---
+++ backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'we4b',
+      username: 'root',
+      password: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- src/data/index.ts
+++
@@ -1,7 +0,0 @@
-import * as _settings from "./settings.json";
-export interface Settings {
-  restaurants_tags: string[];
-  restaurants_per_page: number;
-}
-
-export const settings = _settings as Settings;
